5f1dee56f20c8d06b9eb08e9354d6089ba8ecf6b
Glauco Fox <glauco.fox@gmail.com>
2020-11-03 19:03:49 -0300

    Added User Interface

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from '../../models/User';
 
 @Component({
     selector: 'app-user',
@@ -9,48 +10,18 @@ import { Component } from '@angular/core';
 export class UserComponent 
 {
 
-    firstName: String;
-    lastName: String;
-    address: object;
-    age: number;
-    foo: any;
-    hasKids: Boolean;
-    numberArray: number[];
-    stringArray: string[];
-    mixedArray: any[];
-    myTuple: [string, number, boolean];
-    unusable: void;
-    u: undefined;
-    n: null;
+    user: User;
 
     constructor() {
-        this.firstName = 'John';
-        this.lastName = 'Doe';
-        this.age = 30;
-        this.address = {
-            street: '50 Main St.',
-            city: 'Boston',
-            state: 'MA'
-        };
-        this.foo = true;
-        this.hasKids = false;
-        this.numberArray = [1,2,3];
-        this.stringArray = ['hello', 'world'];
-        this.mixedArray = [true, undefined, 'foo', 13];
-        this.myTuple = ['hello', 13, true];
-        this.unusable = undefined;
-        this.u = undefined;
-        this.n = null;
-
-        console.log(this.addNumbers(2,3));
+        this.user = {
+            firstName: 'John',
+            lastName: 'Doe',
+            age: 30,
+            address: {
+                street: '50 Main st.',
+                city: 'Boston',
+                state: 'MA'
+            }
+        }
     }
-
-    showAge(): number {
-        return this.age;
-    }
-
-    addNumbers(num1: number, num2: number): number {
-        return num1 + num2;
-    }
-
 }
