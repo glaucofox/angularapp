f32794a6bc55e997eb75e3015945e2b674160474
Glauco Fox <glauco.fox@gmail.com>
2020-11-03 19:47:34 -0300

    ngFor directive

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/User';
 
 @Component({
   selector: 'app-users',
@@ -7,10 +8,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+    users: User[];
 
-  ngOnInit(): void {
-      
-  }
+    constructor() { }
+
+    ngOnInit(): void {
+        this.users = [
+            {
+                firstName: 'John',
+                lastName: 'Doe',
+                age: 30,
+                address: {
+                    street: '50 Main st.',
+                    city: 'Boston',
+                    state: 'MA'
+                }
+            },
+            {
+                firstName: 'Kevin',
+                lastName: 'Johnson',
+                age: 34,
+                address: {
+                    street: '20 School st.',
+                    city: 'Lynn',
+                    state: 'MA'
+                }
+            },
+            {
+                firstName: 'Karen',
+                lastName: 'Williams',
+                age: 26,
+                address: {
+                    street: '55 Mill st.',
+                    city: 'Miami',
+                    state: 'FL'
+                }
+            }
+        ]
+
+        this.addUser({
+            firstName: 'David',
+            lastName: 'Jackson',
+            age: 44,
+            address: {
+                street: '12 Wake st.',
+                city: 'Miami',
+                state: 'FL'
+            }
+        })
+    }
+
+    addUser(user: User) {
+        this.users.push(user);
+    }
 
 }
