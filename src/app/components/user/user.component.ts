ef165e525239774890ea3ecc2fb92a3727049f27
Glauco Fox <glauco.fox@gmail.com>
2020-11-03 18:43:18 -0300

    Added types

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -9,21 +9,48 @@ import { Component } from '@angular/core';
 export class UserComponent 
 {
 
-    firstName = 'John';
-    lastName = 'Smith';
-    address = {
-        street: '50 Main St.',
-        city: 'Boston',
-        state: 'MA'
-    }
-    age = 30;
+    firstName: String;
+    lastName: String;
+    address: object;
+    age: number;
+    foo: any;
+    hasKids: Boolean;
+    numberArray: number[];
+    stringArray: string[];
+    mixedArray: any[];
+    myTuple: [string, number, boolean];
+    unusable: void;
+    u: undefined;
+    n: null;
 
     constructor() {
+        this.firstName = 'John';
+        this.lastName = 'Doe';
+        this.age = 30;
+        this.address = {
+            street: '50 Main St.',
+            city: 'Boston',
+            state: 'MA'
+        };
+        this.foo = true;
+        this.hasKids = false;
+        this.numberArray = [1,2,3];
+        this.stringArray = ['hello', 'world'];
+        this.mixedArray = [true, undefined, 'foo', 13];
+        this.myTuple = ['hello', 13, true];
+        this.unusable = undefined;
+        this.u = undefined;
+        this.n = null;
 
+        console.log(this.addNumbers(2,3));
     }
 
-    showAge() {
+    showAge(): number {
         return this.age;
+    }
+
+    addNumbers(num1: number, num2: number): number {
+        return num1 + num2;
     }
 
 }
