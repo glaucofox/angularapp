c8279871507ce8439793f0a94f9b25d6344d30bc
Glauco Fox <glauco.fox@gmail.com>
2020-11-03 18:17:27 -0300

    Added new user component

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-user',
+    templateUrl: './user.component.html',
+    styleUrls: ['./user.component.css'],
+
+    /* Another ways of using it inline */
+    // template: '<h2>John Doe</h2>',
+    // styles: [`
+    //     h2 {
+    //         color: blue
+    //     }
+    // `],
+})
+
+export class UserComponent 
+{
+    
+}
